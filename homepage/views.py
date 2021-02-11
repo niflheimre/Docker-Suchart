a23b2c881bbd3e9f142eb3563f99f3006d4221a1
prai0072010 <prai0072010@gmail.com>
2021-02-12 05:27:00 +0700

    add button

--- homepage/views.py
+++ homepage/views.py
@@ -4,6 +4,8 @@ from django.template import loader
 from .models import case
 import snscrape.modules.twitter as sntwitter
 import copy
+import pandas as pd
+import json
 # Create your views here.
 
 def index(request):
@@ -45,23 +47,49 @@ def twitterSearch(request):
 
     if request.method == 'POST':
         data = request.POST
-        model = {
-            "user":"",
-            "text":""
-        }
         resultlist = {"result" : []}
-
-        for i,tweet in enumerate(sntwitter.TwitterSearchScraper(data['keyword'] + 'since:2015-12-17 until:2020-09-25').get_items()) :
-            if i > int(data['maxtweets']) :
-                break
-
-            jstweet = copy.deepcopy(model)
-            jstweet['user'] = tweet.username
-            jstweet['text'] = tweet.content
-            resultlist['result'].append(jstweet)
-
+        model = {
+                "user":"",
+                "text":"",
+                "index":""
+            }
         template = loader.get_template('homepage/twitter_search.html')
-        '''context = {
-            'text': resultlist,
-        }'''
+        if 'searchBtn' in data :
+            for i,tweet in enumerate(sntwitter.TwitterSearchScraper(data['keyword'] + '(from:' + data['username'] + ')' +'since:2015-12-17').get_items()) :
+                if i > int(data['maxtweets']) :
+                    break
+
+                jstweet = copy.deepcopy(model)
+                jstweet['index'] = i
+                jstweet['user'] = tweet.username
+                jstweet['text'] = tweet.content
+                resultlist['result'].append(jstweet)
+
+        elif 'saveBtn' in data :
+            saved_df = pd.DataFrame(columns=['user_name','text'])
+            coluser = data.getlist('coluser')
+            coltext = data.getlist('coltext')
+            for index in range(len(data.getlist('coluser'))) :
+                jstweet = copy.deepcopy(model)
+                jstweet['index'] = index
+                jstweet['user'] = coluser[index]
+                jstweet['text'] = coltext[index]
+                resultlist['result'].append(jstweet)
+                if  'colindex' in data :
+                    if str(index) in data.getlist('colindex') :
+                        saved_df = saved_df.append({'user_name': coluser[index], 'text': coltext[index]},ignore_index=True)
+            print(saved_df)
+            
+        elif 'saveAll' in data :
+            saved_df = pd.DataFrame(columns=['user_name','text'])
+            coluser = data.getlist('coluser')
+            coltext = data.getlist('coltext')
+            for index in range(len(data.getlist('coluser'))) :
+                jstweet = copy.deepcopy(model)
+                jstweet['index'] = index
+                jstweet['user'] = coluser[index]
+                jstweet['text'] = coltext[index]
+                resultlist['result'].append(jstweet)
+                saved_df = saved_df.append({'user_name': coluser[index], 'text': coltext[index]},ignore_index=True)
+            print(saved_df)
         return HttpResponse(template.render(resultlist, request))
