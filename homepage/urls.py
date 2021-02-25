b92b976f7f6000eb32ca26e192334288302cef72
prai0072010 <prai0072010@gmail.com>
2021-02-25 23:23:19 +0700

    prai gogo

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import include, url
 urlpatterns = [
     path('', views.index, name='index'),
     path('new_case/', views.raiseCase, name='raiseCase'),
+    path('twitter_search/', views.twitterSearch, name='twitterSearch'),
     path('api/query', views.caseExist),
     path('api/model', views.MLmodel),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 from .models import case
 from rest_framework import status
 from django.http.response import JsonResponse
+import snscrape.modules.twitter as sntwitter
+import copy
+import pandas as pd
 
 # Create your views here.
 
@@ -65,3 +70,57 @@ def caseExist(request):
 
 def MLmodel(request):
     return JsonResponse({"status": "This api is in maintenance"},status=status.HTTP_200_OK)
+
+def twitterSearch(request):
+
+    if request.method == 'GET':
+        return render(request, 'homepage/twitter_search.html')
+
+    if request.method == 'POST':
+        data = request.POST
+        resultlist = {"result" : []}
+        model = {
+                "user":"",
+                "text":"",
+                "index":""
+            }
+        template = loader.get_template('homepage/twitter_search.html')
+        if 'searchBtn' in data :
+            for i,tweet in enumerate(sntwitter.TwitterSearchScraper(data['keyword'] + '(from:' + data['username'] + ')' +'since:2015-12-17').get_items()) :
+                if i > int(data['maxtweets']) :
+                    break
+
+                jstweet = copy.deepcopy(model)
+                jstweet['index'] = i
+                jstweet['user'] = tweet.username
+                jstweet['text'] = tweet.content
+                resultlist['result'].append(jstweet)
+
+        elif 'saveBtn' in data :
+            saved_df = pd.DataFrame(columns=['user_name','text'])
+            coluser = data.getlist('coluser')
+            coltext = data.getlist('coltext')
+            for index in range(len(data.getlist('coluser'))) :
+                jstweet = copy.deepcopy(model)
+                jstweet['index'] = index
+                jstweet['user'] = coluser[index]
+                jstweet['text'] = coltext[index]
+                resultlist['result'].append(jstweet)
+                if  'colindex' in data :
+                    if str(index) in data.getlist('colindex') :
+                        saved_df = saved_df.append({'user_name': coluser[index], 'text': coltext[index]},ignore_index=True)
+            print(saved_df)
+            
+        elif 'saveAll' in data :
+            saved_df = pd.DataFrame(columns=['user_name','text'])
+            coluser = data.getlist('coluser')
+            coltext = data.getlist('coltext')
+            for index in range(len(data.getlist('coluser'))) :
+                jstweet = copy.deepcopy(model)
+                jstweet['index'] = index
+                jstweet['user'] = coluser[index]
+                jstweet['text'] = coltext[index]
+                resultlist['result'].append(jstweet)
+                saved_df = saved_df.append({'user_name': coluser[index], 'text': coltext[index]},ignore_index=True)
+            print(saved_df)
+        return HttpResponse(template.render(resultlist, request))
